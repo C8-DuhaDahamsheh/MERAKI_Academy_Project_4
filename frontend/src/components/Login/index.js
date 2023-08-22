f8f3f90ece6cd1be2bf31a239f019d6d047b549f
DuhaDahamsheh <duhadahamsheh@gmail.com>
2023-08-22 16:44:20 +0300

    proj4com15

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState, useContext } from "react";
+import { userContext } from "../../App";
 
- const Login = () => {
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { setToken } = useContext(userContext);
+
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <h1>Login</h1>
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button onClick={async()=>{
+        try {
+            axios.post("http://localhost:5000/users/login", { email, password })
+        } catch (error) {
+            
+        }
+      }}>Login</button>
+    </div>
+  );
+};
 
-
-export default Login
+export default Login;
